f3edfba309578d1943ee48deff44bc140a078f61
chiragshokeen <chirag.shokeen007@gmail.com>
2022-01-03 20:32:39 +0530

    buying feature added

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -10,7 +10,7 @@ contract Marketplace{
         uint id ;
         string name ;
         uint price ; 
-        address owner ; 
+        address payable owner ; 
         bool purchased ;  
 
     }
@@ -19,7 +19,17 @@ contract Marketplace{
         uint id,
         string name,
         uint price,
-        address owner, 
+        address payable owner , 
+        bool purchased   
+
+     ) ;
+
+      event ProductPurchased(
+
+        uint id,
+        string name,
+        uint price,
+        address payable owner , 
         bool purchased   
 
      ) ;
@@ -38,9 +48,62 @@ contract Marketplace{
         //increment the productcount
         productCount++ ;
         //create the product
-        products[productCount] = Product(productCount , _name , _price, msg.sender,false ) ; 
+        products[productCount] = Product(productCount , _name , _price, msg.sender ,false ) ; 
         //trigger an event
-        emit ProductCreated(productCount , _name , _price, msg.sender,false) ; 
+        emit ProductCreated(productCount , _name , _price, msg.sender ,false) ; 
+
+    }
+
+
+    function purchaseProduct(uint _id) public payable{
+
+        //fetch the product
+
+        Product memory _product = products[_id] ; 
+
+        //fetch the owner
+
+        address payable _seller = _product.owner ;
+
+        // make sure the product is valid 
+        //1.valid id
+        require( _product.id > 0 && _product.id <= productCount ) ; 
+
+        //2. there are enough ether in transaction
+        require(msg.value >= _product.price)  ;
+
+        //3. check that product has not been purchased already
+        require( !_product.purchased) ;
+
+        //4. check that seller is not buyer
+        require( _seller != msg.sender ) ;
+
+    
+
+        // transfer ownership to buyer
+
+        _product.owner = msg.sender ; 
+
+        // mark as purchased
+
+        _product.purchased = true ; 
+
+        //update the product
+        products[_id] = _product ;
+
+        // pay the seller by sending them ether
+
+        address(_seller).transfer(msg.value ) ; 
+
+        //trigger an event
+
+        emit ProductPurchased( productCount,_product.name , _product.price ,msg.sender , true ); 
+
+
+
+
+
+
 
     }
 
