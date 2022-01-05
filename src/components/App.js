eb04175de5b6fe34505bea254059535352cf1b70
chiragshokeen <chirag.shokeen007@gmail.com>
2022-01-05 10:48:44 +0530

    5th jan

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 import Marketplace from '../abis/Marketplace.json'
+import Navbar from './Navbar'
+import Main from './Main'
 
 class App extends Component {
 
@@ -47,8 +49,17 @@ class App extends Component {
     const networkData = Marketplace.networks[networkId]
     if(networkData) {
       const marketplace = new web3.eth.Contract(Marketplace.abi, networkData.address)
-      console.log(marketplace)
-     
+      this.setState({ marketplace })
+      const productCount = await marketplace.methods.productCount().call()
+      this.setState({ productCount }) //storting product count
+      // Load products
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call()
+        this.setState({
+          products: [...this.state.products, product]
+        })
+      }
+      this.setState({ loading: false})
    
     } else {
       window.alert('Marketplace contract not deployed to detected network.')
@@ -66,50 +77,43 @@ class App extends Component {
       loading: true
     }
 
+    this.createProduct = this.createProduct.bind(this)
+    this.purchaseProduct = this.purchaseProduct.bind(this)
+
+  }
+
+  createProduct(name, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  purchaseProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
   }
 
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            BLOCKCHAIN MARKETPLACE
-          </a>
-        <ul className='navbar-nav px-3'>
-          <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
-            <small className='text-white'><span id="account">{this.state.account}</span></small>
-          </li>
-        </ul>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
+            <main role="main" className="col-lg-12 d-flex">
+              { this.state.loading
+                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+                : <Main
+                
+                  products={this.state.products}
+                  createProduct={this.createProduct}
+                  purchaseProduct={this.purchaseProduct}
+                   />
+              }
             </main>
           </div>
         </div>
